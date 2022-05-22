72261d03c32d0d67354ca8b8f86799789b37b35f
wony3013 <dnjs3013@naver.com>
2022-05-22 23:58:16 +0900

    파라미터 변수명 수정, 수식검토 기능 구현

--- src/main/java/org/example/Validator.java
+++ src/main/java/org/example/Validator.java
@@ -4,14 +4,14 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-    public void isEmptyCheck(String text){
-        if(text.trim().length() <= 0){
+    public void isEmptyCheck(String formula){
+        if(formula.trim().length() <= 0){
             throw new IllegalArgumentException("입력값이 NULL 이거나 빈공백으로만 이루어져 있는 값입니다.");
         }
     }
     public void signCheck(String sign){
-        if(Pattern.matches("^[0-9 +-*/]")){
-
+        if(sign.matches("[0-9+\\-*/]") ){
+            throw new IllegalArgumentException("사칙연산에 어긋난 값입니다.");
         }
     }
 
