3afe4c40a1daa72d036ae8c317a35a21f4364aa1
wony3013 <dnjs3013@naver.com>
2022-05-26 00:36:44 +0900

    계산 로직 추가(구현중)

--- src/main/java/org/example/InputHandler.java
+++ src/main/java/org/example/InputHandler.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class InputHandler {
 
     public String[] splitText(String text){
         return text.split(" ");
+    }
+
+    public int calculation(String[] splitedText){
+        Arrays.stream(splitedText).iterator().hasNext();
+        return 0;
+    }
+
+    public String[] apply(String[] splitedText){
+        Calculator calculator = new Calculator();
+//        return calculator.calculator(sign, number1, number2);
+        return null;
     }
 
 
