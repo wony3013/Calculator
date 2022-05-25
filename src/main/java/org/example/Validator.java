fd4872a35fc4ae4a69700c7ab7ac0529171e65ef
wony3013 <dnjs3013@naver.com>
2022-05-26 00:39:02 +0900

    Validation 로직 수정 - 정규표현식, 오류 수정

--- src/main/java/org/example/Validator.java
+++ src/main/java/org/example/Validator.java
@@ -9,12 +9,13 @@ public class Validator {
             throw new IllegalArgumentException("입력값이 NULL 이거나 빈공백으로만 이루어져 있는 값입니다.");
         }
     }
-    public void signCheck(String sign){
-        if(sign.matches("[0-9+\\-*/]") ){
+    public void expressionCheck(String formula){
+        String regEx = "[^\\d+\\-*/ ]";
+        Pattern pattern = Pattern.compile(regEx);
+        if(pattern.matcher(formula).find() ){
             throw new IllegalArgumentException("사칙연산에 어긋난 값입니다.");
         }
     }
 
-    // 숫자 + 숫자
 
 }
